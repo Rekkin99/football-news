bb76c6568f457f76877978ad1983248d6ba76162
Rekkin99 <farrellbagoes04@gmail.com>
2025-09-24 13:43:13 +0700

    Tutorial 4 Done

--- main/views.py
+++ main/views.py
@@ -48,6 +48,27 @@ def create_news(request):
 
     return render(request, "create_news.html", context)
 
+# Edit News
+@login_required(login_url='/login')
+def edit_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    form = NewsForm(request.POST or None, instance=news)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_news.html", context)
+
+# Delete News
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
+
 @login_required(login_url='/login')
 def show_news(request, id):
     news = get_object_or_404(News, pk=id)
